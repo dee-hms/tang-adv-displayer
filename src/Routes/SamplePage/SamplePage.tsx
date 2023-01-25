e3289014180ff027b11e696ca252860f1745e58e
Sergio Arroutbi <sarroutb@redhat.com>
2023-01-25 13:04:42 +0100

    Set namespace as different variable
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- src/Routes/SamplePage/SamplePage.tsx
+++ src/Routes/SamplePage/SamplePage.tsx
@@ -22,15 +22,24 @@ const SamplePage = () => {
   const dispatch = useDispatch();
   const [totalReactPackages, setTotalReactPackages] = useState(null);
   const [advUrl, setAdvUrl] = useState("");
-  const API_PATH = "api/v1/namespaces/ephemeral-rfymfc/endpoints/tang-backend-tang";
-  const COMPLETE_URL = "https://api.c-rh-c-eph.8p0c.p1.openshiftapps.com:6443/" + API_PATH;
   const DEFAULT_PORT = 8000;
   // TODO: Specify token from outside
-  const TOKEN = 'YOUR_TOKEN_HERE';
+  const TOKEN = "YOUR_TOKEN_HERE";
   const BEARER_TOKEN = "Bearer " + TOKEN;
+  // TODO: Specify namespace from outside
+  const NAMESPACE= "YOUR_NAMESPACE_HERE";
+  const API_PATH = "api/v1/namespaces/" + NAMESPACE + "/endpoints/tang-backend-tang";
+  // TODO: specify host from outside
+  const COMPLETE_URL = "https://api.c-rh-c-eph.8p0c.p1.openshiftapps.com:6443/" + API_PATH;
 
   useEffect(() => {
     insights?.chrome?.appAction?.('tang-adv-displayer');
+    console.log("Process env:" + process.env);
+    console.log("Process env NODE_ENV:" + process.env.NODE_ENV);
+    console.log("Process env REACT_APP_CLIENT_TOKEN:" + process.env.REACT_APP_CLIENT_TOKEN);
+    console.log("Process env PROXY:" + process.env.PROXY);
+    console.log("Process env DEVELOPMENT TEST:" + process.env.REACT_APP_DEVELOPMENT_TEST);
+    console.log("Process env  PRODUCTION TEST:" + process.env.REACT_APP_PRODUCTION_TEST);
 
     axios.get(COMPLETE_URL,
               {
@@ -67,6 +76,7 @@ const SamplePage = () => {
               <StackItem>
                 <p>Advertisement URL: <a href={advUrl}>{advUrl}</a></p>
                 <p>API URL:{COMPLETE_URL}</p>
+                <p>TOKEN:{BEARER_TOKEN}</p>
               </StackItem>
             </Stack>
           </StackItem>
